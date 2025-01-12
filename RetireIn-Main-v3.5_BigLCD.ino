3216c6341f7d23219cd6351ef453e2ec6199cfd3
R1RF23 <158690629+R1RF23@users.noreply.github.com>
2025-01-12 05:41:17 +0000

    v3.5_LCD-I2C-20x4_RTC-DS3231

--- RetireIn-Main-v3.5_BigLCD.ino
+++ RetireIn-Main-v3.5_BigLCD.ino
@@ -3,14 +3,14 @@
 #include <RTClib.h>
 
 // >>>>>>>>>>> HardWare: Nano-mega328p-_#_-LCD-I2C-20x4-_#_-RTC-DS3231-_#_-2-pin-Button <<<<<<<<<<<<<<<<<
+//             Copyright (C) 2025. Simon Clarke & SRC Repairs ltd. All Rights Reserved
 
- // Copyright (C) 2025. Simon Clarke & SRC Repairs ltd. All Rights Reserved
-  //* This program is free and Open-Source software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
- //as published by the Free Software Foundation, either version 3 of the License, or any later version.
-  //* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
- //implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- //* You should have received a copy of the GNU General Public License along with this program. 
- //If not, see <http://www.gnu.org/licenses/>.
+ // This program is free and Open-Source software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+ // as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ // implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ // You should have received a copy of the GNU General Public License along with this program. 
+ // If not, see <http://www.gnu.org/licenses/>.
 
 RTC_DS3231 rtc;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
